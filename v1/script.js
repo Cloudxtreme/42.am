d9dc91ea87ddfe2852afd4b3082b90fb9d4afa96
Manfred Touron <github@42.am>
2012-02-04 22:10:07 +0100

    changed analytics code

--- v1/script.js
+++ v1/script.js
@@ -182,13 +182,11 @@ $(function() {
 /* Google Analytics */
 
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-28926229-1']);
-_gaq.push(['_setDomainName', 'onouo.com']);
+_gaq.push(['_setAccount', 'UA-28089933-1']);
+_gaq.push(['_setDomainName', '42.am']);
 _gaq.push(['_trackPageview']);
-
 (function() {
      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
  })();
-
